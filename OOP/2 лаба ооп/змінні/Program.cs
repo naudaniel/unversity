28d290f397a6cc972c954ff788b649c5e35be81d
naudaniel <77957930+naudaniel@users.noreply.github.com>
2021-02-02 14:21:11 +0200

    Add files via upload

---
+++ OOP/2 лаба ооп/змінні/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace var
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float f = 1.59f, w = 8.5f;
+            double pi = 3.14;
+            Console.WriteLine("f={0} l={1} pi={2}", f, w, pi);
+
+            //Pause for console
+            Console.ReadKey();
+        }
+    }
+}
---
+++ OOP/2 лаба ооп/операції/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace operation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int q = 2;
+            Console.WriteLine(q);
+            q++;
+            Console.WriteLine(q);
+            q += 1;
+            Console.WriteLine(q);
+            q--;
+            Console.WriteLine(q);
+
+            Console.ReadKey();
+        }
+    }
+}
